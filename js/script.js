11f2a1d1a247d462a4c1da4e18303903d889b7d6
kzelenovic <106201960+kzelenovic@users.noreply.github.com>
2022-06-17 11:36:13 -0700

    completed showPage function as well as addPagination
    made the showPage function more simple by using template literals. created the addPagination function to add links at the bottom to navigate through the student data.

--- js/script.js
+++ js/script.js
@@ -1,57 +1,16 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
+// Data Pagination & Filtering
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
-function createElement(elementName, property, value) {
-   const element = document.createElement(elementName);
-   element[property] = value;
-   return element;
-}
+// showPage function creates and appends elements to display a page of 9 students
 
 const perPage = 9;
 
-function showPage (list, page) {
+function showPage(list, page) {
    const startIndex = ( page * perPage) - perPage;
    const endIndex = page * perPage;
    const studentList = document.querySelector('.student-list');
-   console.log(studentList);
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i++){
       if (i >= startIndex && i < endIndex) {
-/*          const studentLI = createElement('li', 'className', 'student-item cf');
-         studentList.appendChild(studentLI);
-         const detailsDiv = createElement('div', 'className', 'student-details');
-         studentLI.appendChild(detailsDiv);
-         const studentImg = createElement('img', 'className', 'avatar');
-         studentImg.src = data[i].picture.large;
-         studentImg.alt = 'Profile Picture';
-         detailsDiv.appendChild(studentImg);
-         const studentName = createElement('h3');
-         studentName.textContent = `${data[i].name.first} ${data[i].name.last}`;
-         detailsDiv.appendChild(studentName);
-         const studentEmail = createElement('span', 'className', 'email');
-         studentEmail.textContent = data[i].email;
-         detailsDiv.appendChild(studentEmail);
-         const joinedDiv = createElement('div', 'className', 'joined-details');
-         studentLI.appendChild(joinedDiv);
-         const dateSpan = createElement('span', 'className', 'date');
-         dateSpan.textContent = data[i].registered.date;
-         joinedDiv.appendChild(dateSpan); */
          studentList.insertAdjacentHTML(
             'beforeend',
             `<li class="student-item cf">
@@ -69,13 +28,43 @@ function showPage (list, page) {
    }
 }
 
-showPage(data, 1);
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+/* addPagination function creates buttons at the bottom to use as navigation through the student pages
+includes an event listener and setActive function to change the style of the button when clicked */
 
+function addPagination(list) {
+   const numberOfPages = list.length / perPage;
+   const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
 
+   for (let i = 0; i < numberOfPages; i++) {
+      linkList.insertAdjacentHTML(
+         'beforeend',
+         `<li>
+            <button type="button">${i+1}</button>
+         </li>`
+      );
+   }
+   
+   const pageButtons = document.querySelectorAll('button');
+   pageButtons[0].className = 'active';
 
-// Call functions
+   function setActive(e) {
+      for (let i = 0; i < pageButtons.length; i++) {
+         pageButtons[i].classList.remove('active');
+      }
+      const clicked = e.target;
+      clicked.classList.add('active');
+   }
 
+   linkList.addEventListener('click', (e) => {
+      if (e.target.tagName === 'BUTTON'){
+         setActive(e);
+         showPage(data, e.target.textContent)
+      }
+   });
+}
+
+// call functions starting with showPage so it loads with page 1 ready
+
+showPage(data, 1)
+addPagination(data);
